734acf3d1ee756f46a79e32265453b18c854b006
i_polugrudov <ipolugrudov@gmail.com>
2024-09-05 23:51:34 +0300

    форма

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 
 function App() {
   return (
-   <>
-   <p>Hello</p>
-   </>
+    <div class="login-container">
+    <h2>Авторизация</h2>
+    <form class="login-form">
+      <input type="text" placeholder="Логин" required/>
+      <input type="password" placeholder="Пароль" required/>
+      <button type="submit">Вход</button>
+    </form>
+  </div>
   );
 }
 
